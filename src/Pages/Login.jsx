9a2b1b8c10c14182b639407d4ee3bca275808037
Saurabh-404 <souravpandey949@gmail.com>
2025-06-16 22:44:50 +0530

    Updated login pages and fixes some errors

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -29,7 +29,7 @@ const login = () => {
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                             className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="you@example.com"
+                            placeholder="you@gmail.com"
                         />
                     </div>
                     <div>
