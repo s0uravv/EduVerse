fb3d09ab920c54876df739d2b36b02f48b04415e
Saurabh-404 <souravpandey949@gmail.com>
2025-06-26 14:37:20 +0530

    update the herosection

--- client/src/Components/HeroSection/FeatureSection.jsx
+++ client/src/Components/HeroSection/FeatureSection.jsx
@@ -37,10 +37,17 @@ const FeatureSection = () => {
             </section>
 
             {/* Section 2 */}
-            
-            
+
             <section className="w-full bg-[#0b0625] px-6 py-12">
-                <div className="flex flex-col lg:flex-row-reverse items-center justify-between max-w-6xl mx-auto gap-10">
+                <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-10">
+                    {/* Image */}
+                    <div className="w-full lg:w-1/2 flex justify-center items-center">
+                        <img
+                            className="w-[80%] max-h-[350px] object-contain"
+                            alt="Phone"
+                            src="/iPhone.png"
+                        />
+                    </div>
                     {/* Text */}
                     <div className="w-full lg:w-1/2  lg:text-left text-white">
                         <h2 className="text-3xl md:text-4xl font-bold text-orange-400 mb-4">
@@ -58,29 +65,22 @@ const FeatureSection = () => {
                             Learn More ↗
                         </a>
                     </div>
-
-                    {/* Image */}
-                    <div className="w-full lg:w-1/2 flex justify-center items-center">
-                        <img
-                            className="w-[80%] max-h-[350px] object-contain"
-                            alt="Phone"
-                            src="/iPhone.png"
-                        />
-                    </div>
                 </div>
             </section>
-            
-            
 
             <section className="w-full bg-white px-6 py-12">
-                <div className="flex flex-col lg:flex-row items-center justify-between max-w-6xl mx-auto gap-10">
+                <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-10">
                     {/* Text */}
                     <div className="w-full lg:w-1/2 lg:text-left">
                         <h2 className="text-3xl md:text-4xl font-bold text-orange-500 mb-4">
                             AI Chat Bot
                         </h2>
                         <p className="max-w-2xl font-bold mx-auto text-black md:text-lg pb-4">
-                            The AI Chat Bot in EduVerse is a assistant designed to support students and teachers by answering questions, providing explanations, suggesting study tips, and helping with homework or concepts — all in a natural chat interface.
+                            The AI Chat Bot in EduVerse is a assistant designed
+                            to support students and teachers by answering
+                            questions, providing explanations, suggesting study
+                            tips, and helping with homework or concepts — all in
+                            a natural chat interface.
                         </p>
                         <a
                             href="#"
@@ -101,27 +101,8 @@ const FeatureSection = () => {
                 </div>
             </section>
 
-
-
-
             <section className="w-full bg-white px-6 py-12">
-                <div className="flex flex-col lg:flex-row-reverse items-center justify-between max-w-6xl mx-auto gap-10">
-                    {/* Text */}
-                    <div className="w-full lg:w-1/2  lg:text-left text-white">
-                        <h2 className="text-3xl md:text-4xl font-bold text-orange-400 mb-4">
-                            Study Material
-                        </h2>
-                        <p className="max-w-2xl text-black font-bold mx-auto md:text-lg mb-4">
-                            The Study Material section in EduVerse is a centralized digital library for students and teachers. It provides quick access to notes, NCERT PDFs, practice papers, video lectures, and teacher-uploaded resources.
-                        </p>
-                        <a
-                            href="#"
-                            className="text-black hover:text-orange-300 underline decoration-orange-500 underline-offset-4 font-semibold inline-flex items-center gap-1"
-                        >
-                            Learn More ↗
-                        </a>
-                    </div>
-
+                <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto gap-10">
                     {/* Image */}
                     <div className="w-full lg:w-1/2 flex justify-center items-center">
                         <img
@@ -129,6 +110,26 @@ const FeatureSection = () => {
                             alt="Phone"
                             src="/iPhone.png"
                         />
+                    </div>
+
+                    {/* Text */}
+                    <div className="w-full lg:w-1/2  lg:text-left text-white">
+                        <h2 className="text-3xl md:text-4xl font-bold text-orange-400 mb-4">
+                            Study Material
+                        </h2>
+                        <p className="max-w-2xl text-black font-bold mx-auto md:text-lg mb-4">
+                            The Study Material section in EduVerse is a
+                            centralized digital library for students and
+                            teachers. It provides quick access to notes, NCERT
+                            PDFs, practice papers, video lectures, and
+                            teacher-uploaded resources.
+                        </p>
+                        <a
+                            href="#"
+                            className="text-black hover:text-orange-300 underline decoration-orange-500 underline-offset-4 font-semibold inline-flex items-center gap-1"
+                        >
+                            Learn More ↗
+                        </a>
                     </div>
                 </div>
             </section>
